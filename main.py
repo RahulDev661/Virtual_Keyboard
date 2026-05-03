dbae1738f31d8203c69c67921a1fe943487d23e1
Rahul Dev <rahuldevbera098@gmail.com>
2026-05-03 08:00:00 +0530

    Updated keyboard UI

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def drawAll(img, buttonList):
 # KEYBOARD LAYOUT
 # =========================
 keys = [
-    ["Q","W","E","R","T","Y","U","I","O","P"],
+    ["Q","8","E","R","T","Y","U","I","O","P"],
     ["A","S","D","F","G","H","J","K","L"],
     ["Z","X","C","V","B","N","M"]
 ]
